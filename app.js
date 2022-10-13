b682d3f5744536121bf012266b08798fd8082f6b
Shivam Tyagi <mergesuccessful@gmail.com>
2022-10-13 12:13:31 +0530

    database connected
    Signed-off-by: Shivam Tyagi <mergesuccessful@gmail.com>

---
+++ app.js
@@ -0,0 +1,12 @@
+// console.log("hey backend expert")
+require("dotenv").config();
+require("./config/database").connect();
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+// Logic goes here
+
+module.exports = app;
