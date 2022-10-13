9748bc44ef099b1ab1373bd1e62b33c3dd38ee2f
Shivam Tyagi <mergesuccessful@gmail.com>
2022-10-13 15:15:34 +0530

    create and read endpoints
    Signed-off-by: Shivam Tyagi <mergesuccessful@gmail.com>

--- app.js
+++ app.js
@@ -4,50 +4,77 @@ require("./config/database").connect();
 // importing user context
 const User = require("./model/user");
 const express = require("express");
-const bcrypt = require("bcryptjs")
-const jwt = require("jsonwebtoken")
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const auth = require("./middleware/auth");
 
 const app = express();
 
 app.use(express.json());
 
+app.post("/create", async (req, res) => {
+  // Our signup logic starts here
+  try {
+    // Get user input
+    const { first_name, last_name, email, password } = req.body;
 
-app.get("/",(req,res)=>{
-    res.send(" Home Page")
-})
+    // Validate user input
+    if (!(email && password && first_name && last_name)) {
+      res.status(400).send("All input is required");
+    }
 
-app.post("/signup", async (req, res) => {
+    // check if user already exist
+    // Validate if user exist in our database
+    const oldUser = await User.findOne({ email });
 
-    // Our signup logic starts here
-    try {
-      // Get user input
-      const { first_name, last_name, email, password } = req.body;
-  
-      // Validate user input
-      if (!(email && password && first_name && last_name)) {
-        res.status(400).send("All input is required");
+    if (oldUser) {
+      return res.status(409).send("User Already Exist. Please Login");
+    }
+
+    //Encrypt user password
+    encryptedPassword = await bcrypt.hash(password, 10);
+
+    // Create user in our database
+    const user = await User.create({
+      first_name,
+      last_name,
+      email: email.toLowerCase(),
+      password: encryptedPassword,
+    });
+
+    // Create token
+    const token = jwt.sign(
+      { user_id: user._id, email },
+      process.env.TOKEN_KEY,
+      {
+        expiresIn: "2h",
       }
-  
-      // check if user already exist
-      // Validate if user exist in our database
-      const oldUser = await User.findOne({ email });
-  
-      if (oldUser) {
-        return res.status(409).send("User Already Exist. Please Login");
-      }
-  
-      //Encrypt user password
-      encryptedPassword = await bcrypt.hash(password, 10);
-  
-      // Create user in our database
-      const user = await User.create({
-        first_name,
-        last_name,
-        email: email.toLowerCase(), // sanitize: convert email to lowercase
-        password: encryptedPassword,
-      });
-  
+    );
+    // save user token
+    user.token = token;
+
+    // return new user
+    res.status(201).json(user);
+  } catch (err) {
+    console.log(err);
+  }
+  // Our signup logic ends here
+});
+
+app.post("/login", async (req, res) => {
+  // Our login logic starts here
+  try {
+    // Get user input
+    const { email, password } = req.body;
+
+    // Validate user input
+    if (!(email && password)) {
+      res.status(400).send("All input is required");
+    }
+    // Validate if user exist in our database
+    const user = await User.findOne({ email });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
       // Create token
       const token = jwt.sign(
         { user_id: user._id, email },
@@ -56,58 +83,40 @@ app.post("/signup", async (req, res) => {
           expiresIn: "2h",
         }
       );
+
       // save user token
       user.token = token;
-  
-      // return new user
-      res.status(201).json(user);
-    } catch (err) {
-      console.log(err);
+
+      // user
+      res.status(200).json(user);
+      ``;
     }
-    // Our signup logic ends here
-  });
+    res.status(400).send("Invalid Credentials");
+  } catch (err) {
+    console.log(err);
+  }
+  // Our login logic ends here
+});
 
-  app.post("/login", async (req, res) => {
+app.post("/welcome", auth, (req, res) => {
+  res.status(200).send("Welcome 🙌 ");
+});
 
-    // Our login logic starts here
-    try {
-      // Get user input
-      const { email, password } = req.body;
-  
-      // Validate user input
-      if (!(email && password)) {
-        res.status(400).send("All input is required");
-      }
-      // Validate if user exist in our database
-      const user = await User.findOne({ email });
-  
-      if (user && (await bcrypt.compare(password, user.password))) {
-        // Create token
-        const token = jwt.sign(
-          { user_id: user._id, email },
-          process.env.TOKEN_KEY,
-          {
-            expiresIn: "2h",
-          }
-        );
-  
-        // save user token
-        user.token = token;
-  
-        // user
-        res.status(200).json(user);
-      }
-      res.status(400).send("Invalid Credentials");
-    } catch (err) {
-      console.log(err);
+// Read all the existent users in database
+
+app.get("/read", (req, res) =>
+  User.find({}, (err, users) => {
+    if (err) {
+      res.send("Error when fetching users.");
+      next();
     }
-    // Our login logic ends here
-  });
-
-
-  app.post("/welcome", auth, (req, res) => {
-    res.status(200).send("Welcome 🙌 ");
-  });
+    res.send(
+      users.reduce((userMap, item) => {
+        userMap[item.id] = item;
+        return userMap;
+      }, {})
+    );
+  })
+);
 
 module.exports = app;
-
