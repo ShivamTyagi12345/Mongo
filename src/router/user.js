9d8cdff49c93934c1a81ba642fd0b244de77d0a3
Shivam Tyagi <mergesuccessful@gmail.com>
2022-10-16 04:15:40 +0530

    delete and update route

--- src/router/user.js
+++ src/router/user.js
@@ -3,55 +3,63 @@
  * @param {FastifyInstance} fastify encapsulated fastify instance
  * @param {Object} options plugin options, refer to https://www.fastify.io/docs/latest/Reference/Plugins/#plugin-options
  */
- async function routes (fastify, options) {
-  const collection = fastify.mongo.db.collection('test')
+async function routes(fastify, options) {
+  const collection = fastify.mongo.db.collection("test");
 
-  fastify.get('/', async (request, reply) => {
-    return { hello: 'world' }
-  })
+  fastify.get("/", async (request, reply) => {
+    return { hello: "world" };
+  });
 
-  fastify.get('/read', async (request, reply) => {
-    const result = await collection.find().toArray()
+  fastify.get("/read", async (request, reply) => {
+    const result = await collection.find().toArray();
     if (result.length === 0) {
-      throw new Error('No documents found')
+      throw new Error("No documents found");
     }
-    return result
-  })
+    return result;
+  });
 
-  fastify.post('/create', async (request, reply) => {
-    const result = await collection.insert().toArray({
-      name: 'shivam taygi',
-      email:'backend@gmail.com',
-      password:'1234'
-    })
-    return result
-  })
+  fastify.post("/create", async (request, reply) => {
+    // console.log(request.body)
+    const { name, email, password } = request.body;
+    // console.log(name);
+    // console.log(email);
+    // console.log(password);
+    const result = await collection.insert({
+      name,
+      email,
+      password,
+    });
+    return result;
+  });
 
-  fastify.get('/users/:id', async (request, reply) => {
-    const result = await collection.findOne({ animal: request.params._id })
+  fastify.put("/update/:id", async (request, reply) => {
+    // console.log(request.body)
+    const { name, email, password } = request.body;
+    console.log(name);
+    console.log(email);
+    console.log(password);
+    const result = await collection.updateOne(
+      { _id: request.params.id },
+      {
+        $set: { name, email, password },
+      },
+      { upsert: true }
+    );
+    return result;
+  });
+
+  fastify.delete("/delete/:id", async (request, reply) => {
+    const result = await collection.deleteOne({ _id: request.params.id });
+    return result;
+  });
+
+  fastify.get("/users/:id", async (request, reply) => {
+    const result = await collection.findOne({ animal: request.params._id });
     if (!result) {
-      throw new Error('Invalid value')
+      throw new Error("Invalid value");
     }
-    return result
-  })
-
-  const animalBodyJsonSchema = {
-    type: 'object',
-    required: ['animal'],
-    properties: {
-      animal: { type: 'string' },
-    },
-  }
-
-  const schema = {
-    body: animalBodyJsonSchema,
-  }
-
-  fastify.post('/users', { schema }, async (request, reply) => {
-    // we can use the `request.body` object to get the data sent by the client
-    const result = await collection.insertOne({ animal: request.body.animal })
-    return result
-  })
+    return result;
+  });
 }
 
-module.exports = routes
+module.exports = routes;
